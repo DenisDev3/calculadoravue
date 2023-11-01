d315c2a5de8b77a7ebdf57ae9e9e5bb13633ff7b
Denis Oliveira <denisdev3loper@gmail.com>
2023-11-01 04:30:13 -0300

    inicial

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { reactive, ref } from 'vue';
+import Calculista from './components/Calculista.vue' ;
+
+// ----------------------------------------------------------------:
+
+const Calculadora = reactive({
+  campoA: 0,
+  campoB: 0,
+  filtro: ref('multiplicar','dividir','menos','mais'), // Defina o filtro padrão aqui
+});
+
+const mais = () => {
+    return Calculadora.campoA + Calculadora.campoB;
+  }
+
+const menos = () => {
+  return Calculadora.campoA - Calculadora.campoB;
+}
+
+const dividir = () => {
+  return Calculadora.campoA / Calculadora.campoB;
+}
+
+const multiplicar = () => {
+  return Calculadora.campoA * Calculadora.campoB;
+}
+
+const getFiltroCalculo = () => {
+  switch (Calculadora.filtro) {
+    case 'multiplicar':
+      return multiplicar();
+    case 'dividir':
+      return dividir();
+    case 'menos':
+      return menos();
+    case 'mais':
+      return mais();
+    default:
+      return multiplicar();
+  }
+}
+</script>
+
+<template>
+  <div class="container row">
+    <Calculista class="col-md-12" :filtro="getFiltroCalculo()" :-calculadora="Calculadora" :dividir="dividir()" :mais="mais()" :multiplicar="multiplicar()" :menos="menos()"  />
+  </div>
+</template>
+<style scoped>
+img {
+  width: 250px;
+  height: 260px;
+}
+.container{
+  background-image: url(./images/OIG.jpeg);
+}
+</style>
---
+++ src/components/Calculista.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps(['Calculadora','filtro','mais','menos','dividir','multiplicar']);
+</script>
+<template>
+    <div class="row">
+        <div class="col-md-2">
+            <select v-model="props.Calculadora.filtro" class="form-select">
+                <option value="multiplicar">Multiplicar</option>
+                <option value="dividir">Divisão</option>
+                <option value="menos">Subtração</option>
+                <option value="mais">Adição</option>
+            </select>
+            <input type="number" v-model="Calculadora.campoA" placeholder="Digite um número" class="form-control">
+            <input type="number" v-model="Calculadora.campoB" placeholder="Digite um número" class="form-control">
+
+            <p>Resultado: {{  props.filtro }}</p>
+        </div>
+    </div>
+</template>
+<style scoped>
+p{
+    color: white;
+}
+</style>
